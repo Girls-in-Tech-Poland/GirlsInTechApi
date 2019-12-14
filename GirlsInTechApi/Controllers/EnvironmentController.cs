71baa888cad3465dcd6756f5908aebe945fcd768
Maciek Misztal <mmisztal1980@gmail.com>
2019-12-14 15:46:07 +0100

    initial commit

---
+++ GirlsInTechApi/Controllers/EnvironmentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace GirlsInTechApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EnvironmentController : ControllerBase
+    {
+        private readonly IHostEnvironment _hostEnvironment;
+
+        public EnvironmentController(IHostEnvironment hostEnvironment)
+        {
+            _hostEnvironment = hostEnvironment ?? throw new ArgumentNullException(nameof(hostEnvironment));
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_hostEnvironment.EnvironmentName);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# GirlsInTechApi
+
+Contains a starter .NET Core 3.1 to demonstrate App Service deployment
